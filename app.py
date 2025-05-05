35f59dbc43ce50950a9379f903353a537c31bac6
Bathini Rohini <rohinibathini20@gmail.com>
2025-05-05 20:55:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
-from flask import Flask, send_file
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# Route to show the "Coming Soon" message
 @app.route('/')
 def home():
-    return '''
-    <h2>Welcome to Our eBook Page!</h2>
-    <p>The eBook is coming soon. Check back later!</p>
-    '''
-
-# Route for downloading the real eBook (update when you're ready)
-@app.route('/download_ebook')
-def download_ebook():
-    # For now, it's a placeholder message.
-    return "The eBook will be available soon. Stay tuned!"
-
-# Uncomment below code when you're ready to serve the real eBook
-# @app.route('/download_ebook')
-# def download_ebook():
-#     return send_file('static/real_ebook.pdf', as_attachment=True)
+    return "Hello from Flask on Render!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Use Render's assigned port or 10000 for local testing
+    app.run(host='0.0.0.0', port=port)
